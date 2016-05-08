f2248b9649df3dd150ccba9c4fa3157b5c681101
Vadym Chepkov <vchepkov@gmail.com>
2016-05-08 17:06:48 -0400

    run puppet agent to populate puppetdb

--- examples/bootstrap.sh
+++ examples/bootstrap.sh
@@ -3,3 +3,4 @@ DIR=$(dirname $0)
 # To resolve dependency circle install puppet server first, then reconfigure it to use puppetdb
 env FACTER_bootstrap=yes /opt/puppetlabs/bin/puppet apply --modulepath /etc/puppetlabs/code/environments/production/modules ${DIR}/bootstrap.pp
 /opt/puppetlabs/bin/puppet apply --modulepath /etc/puppetlabs/code/environments/production/modules ${DIR}/bootstrap.pp
+/opt/puppetlabs/bin/puppet agent -t
