9f430adce00124fc3e345a11dd2ceff833c3c249
Vadym Chepkov <vchepkov@gmail.com>
2016-05-08 20:28:08 -0400

    prevent vagrant script from failing
    puppet agent -t returns exit code 2 if any changes were made

--- examples/bootstrap.sh
+++ examples/bootstrap.sh
@@ -2,3 +2,4 @@
 DIR=$(dirname $0)
 /opt/puppetlabs/bin/puppet apply --modulepath /etc/puppetlabs/code/environments/production/modules ${DIR}/bootstrap.pp
 /opt/puppetlabs/bin/puppet agent -t
+exit 0
