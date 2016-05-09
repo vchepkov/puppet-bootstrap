47d5840802328f9c4266ed1e14bf710e459ff3d6
Vadym Chepkov <vchepkov@gmail.com>
2016-05-08 21:09:43 -0400

    remove manual agent run, service starts automatically

--- examples/bootstrap.sh
+++ examples/bootstrap.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 DIR=$(dirname $0)
 /opt/puppetlabs/bin/puppet apply --modulepath /etc/puppetlabs/code/environments/production/modules ${DIR}/bootstrap.pp
-/opt/puppetlabs/bin/puppet agent -t
-exit 0
