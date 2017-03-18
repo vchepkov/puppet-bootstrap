7f7814c96acc8dd227694e91ba688031ad97d148
Vadym Chepkov <vchepkov@gmail.com>
2017-03-18 13:20:09 -0400

    fix modulepath

--- examples/bootstrap.sh
+++ examples/bootstrap.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 DIR=$(dirname $0)
-/opt/puppetlabs/bin/puppet apply --modulepath /etc/puppetlabs/code/environments/production/modules ${DIR}/bootstrap.pp $*
+/opt/puppetlabs/bin/puppet apply \
+--modulepath /etc/puppetlabs/code/environments/production/modules:/etc/puppetlabs/code/environments/production/site \
+${DIR}/bootstrap.pp $*
