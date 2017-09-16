98cf7ba03de927d2324fba3544ed463c8fb74357
Vadym Chepkov <vchepkov@gmail.com>
2017-09-15 21:55:36 -0400

    add environment variable

--- site/bootstrap/manifests/mco.pp
+++ site/bootstrap/manifests/mco.pp
@@ -5,9 +5,10 @@ class bootstrap::mco {
   $user = 'vagrant'
 
   exec { 'request mco certificate':
-    command => '/opt/puppetlabs/puppet/bin/mco choria request_cert',
-    creates => "/home/${user}/.puppetlabs/etc/puppet/ssl/certs/${user}.mcollective.pem",
-    user    => $user,
-    require => Class['mcollective'],
+    command     => '/opt/puppetlabs/puppet/bin/mco choria request_cert',
+    creates     => "/home/${user}/.puppetlabs/etc/puppet/ssl/certs/${user}.mcollective.pem",
+    user        => $user,
+    environment => ["HOME=/home/${user}", "USER=${user}"],
+    require     => Class['mcollective_choria'],
   }
 }
