316e8c1b3f691b7f92cf58da0d64f2494c503d8e
Vadym Chepkov <vchepkov@gmail.com>
2018-02-26 21:46:32 -0500

    redundant, deafult for the production environment

--- examples/bootstrap.sh
+++ examples/bootstrap.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 DIR=$(dirname $0)
 /opt/puppetlabs/bin/puppet apply \
---modulepath /etc/puppetlabs/code/environments/production/modules:/etc/puppetlabs/code/environments/production/site \
 ${DIR}/bootstrap.pp $*
