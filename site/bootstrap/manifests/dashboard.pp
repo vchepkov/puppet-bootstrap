89c96506b7fa4a796a6905759d8101f7a52b098d
Vadym Chepkov <vchepkov@gmail.com>
2019-09-14 09:29:22 -0400

    revert to Puppetboard 1.0.0

--- site/bootstrap/manifests/dashboard.pp
+++ site/bootstrap/manifests/dashboard.pp
@@ -1,6 +1,6 @@
 # class to install puppet dashboard
 class bootstrap::dashboard (
-  Optional[String] $version = 'v1.1.0', # https://github.com/voxpupuli/puppetboard
+  Optional[String] $version = 'v1.0.0', # https://github.com/voxpupuli/puppetboard
 ) {
 
   class { 'apache':
@@ -35,6 +35,15 @@ class bootstrap::dashboard (
     extra_settings    => {
       'DAILY_REPORTS_CHART_DAYS' => '10',
     },
+  }
+
+  # FIXME: Workaround for broken pypuppetdb v1.1.0
+  # Can't use file_line because of the dependency cycle
+  exec { 'fix pypuppetdb version':
+    command => '/bin/sed -i "s/pypuppetdb.*/pypuppetdb ==1.0.0/" /opt/puppetboard/puppetboard/requirements.txt',
+    unless  => '/bin/grep "pypuppetdb ==1.0.0" /opt/puppetboard/puppetboard/requirements.txt',
+    require => Vcsrepo['/opt/puppetboard/puppetboard'],
+    before  => Python::Virtualenv['/opt/puppetboard/virtenv-puppetboard'],
   }
 
   class { 'puppetboard::apache::conf':
