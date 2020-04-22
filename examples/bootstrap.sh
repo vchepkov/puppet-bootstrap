223b8d4a3c55d46fdc556b9d7f7a4c6f7b3387c7
Vadym Chepkov <vchepkov@gmail.com>
2020-04-22 12:46:35 -0400

    workaround for Puppet 6 bug

--- examples/bootstrap.sh
+++ examples/bootstrap.sh
@@ -4,6 +4,9 @@ puppet_environment=${PUPPET_ENV:-production}
 
 /opt/puppetlabs/puppet/bin/r10k deploy environment $puppet_environment -vp || exit 1
 
+# Workaround for bug https://tickets.puppetlabs.com/browse/PUP-9602
+rm -rf $(/opt/puppetlabs/bin/puppet config print environmentpath)/${puppet_environment}/.resource_types
+
 /opt/puppetlabs/bin/puppet apply \
 --environment $puppet_environment \
 -e "class { 'bootstrap::master': environment => $puppet_environment }"
