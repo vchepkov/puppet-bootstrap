4d3d60916e3e2e0d92dfa8d4f38a9b43bb1d86e6
Vadym Chepkov <vchepkov@gmail.com>
2020-07-28 15:11:03 -0400

    use r10k generate types deployment option

--- site/bootstrap/files/refresh-environments.sh
+++ site/bootstrap/files/refresh-environments.sh
@@ -4,7 +4,6 @@ CERT=$(puppet config print hostcert)
 KEY=$(puppet config print hostprivkey)
 
 for environment in $1; do
-  /opt/puppetlabs/bin/puppet generate types --environment $environment
   /bin/curl -k --cert $CERT --key $KEY -X DELETE \
     https://localhost:8140/puppet-admin-api/v1/environment-cache\?environment=$environment
 done
