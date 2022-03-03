7a757fd58e2759fff9f6842bd7e40d298a685e6e
Vadym Chepkov <vchepkov@gmail.com>
2022-03-03 18:33:35 -0500

    make sure current directory is readable for choria

--- site/bootstrap/manifests/mco.pp
+++ site/bootstrap/manifests/mco.pp
@@ -5,6 +5,7 @@ class bootstrap::mco (
   $admins.each | $user | {
     exec { "request mco certificate for ${user}":
       command     => '/usr/bin/choria enroll',
+      cwd         => '/',
       creates     => "/home/${user}/.puppetlabs/etc/puppet/ssl/certs/${user}.mcollective.pem",
       user        => $user,
       environment => ["HOME=/home/${user}", "USER=${user}"],
