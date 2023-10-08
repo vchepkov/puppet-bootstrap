c1cbd3807c5a00a91053ec95d7912d1f0a1517c7
Vadym Chepkov <vchepkov@gmail.com>
2023-10-08 12:01:50 -0400

    workaround for el9 tmpfiles restrictions

--- site/bootstrap/manifests/maintenance.pp
+++ site/bootstrap/manifests/maintenance.pp
@@ -1,8 +1,17 @@
 # Puppet maintenance
 class bootstrap::maintenance (
-  Array[String] $clean_dir = [],
-  String $max_age          = '30d',
+  Array[Stdlib::Unixpath] $clean_dir = [],
+  String $user = 'puppet',
+  String $max_age = '30d',
 ) {
+  # systemd refuses clean directories if they are owned by root
+  $clean_dir.each | $dir | {
+    exec { "chown ${user} ${dir}":
+      unless => "test \"\$(stat --printf '%U' ${dir})\" = \"${user}\"",
+      path   => $facts['path'],
+      notify => Systemd::Tmpfile['puppet-maintenance.conf'],
+    }
+  }
   systemd::tmpfile { 'puppet-maintenance.conf':
     content => epp("${module_name}/puppet-maintenance.epp", {
         clean_dir => $clean_dir,
