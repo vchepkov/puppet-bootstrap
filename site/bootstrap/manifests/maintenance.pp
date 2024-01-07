f5c38d5836078bb1e29c23af35a3e1c6208aef80
Vadym Chepkov <vchepkov@gmail.com>
2024-01-07 09:56:14 -0500

    puppet user doesn't exist on nodes

--- site/bootstrap/manifests/maintenance.pp
+++ site/bootstrap/manifests/maintenance.pp
@@ -1,17 +1,19 @@
 # Puppet maintenance
 class bootstrap::maintenance (
   Array[Stdlib::Unixpath] $clean_dir = [],
-  String $user = 'puppet',
   String $max_age = '30d',
+  Optional[String] $user = undef,
 ) {
-  # systemd refuses clean directories if they are owned by root
-  $clean_dir.each | $dir | {
-    exec { "chown ${user} ${dir}":
-      unless => "test \"\$(stat --printf '%U' ${dir})\" = \"${user}\"",
-      path   => $facts['path'],
-      notify => Systemd::Tmpfile['puppet-maintenance.conf'],
+  if $user {
+    $clean_dir.each | $dir | {
+      exec { "install -d -o ${user} ${dir}":
+        unless => "test \"\$(stat --printf '%U' ${dir})\" = \"${user}\"",
+        path   => $facts['path'],
+        before => Systemd::Tmpfile['puppet-maintenance.conf'],
+      }
     }
   }
+
   systemd::tmpfile { 'puppet-maintenance.conf':
     content => epp("${module_name}/puppet-maintenance.epp", {
         clean_dir => $clean_dir,
